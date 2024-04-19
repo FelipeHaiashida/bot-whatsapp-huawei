ae99b6f99f329cdb417aa7da250e0f0b39185506
FelipeHaiashida <112267078+FelipeHaiashida@users.noreply.github.com>
2024-04-19 15:31:22 -0300

    adicionando código para receber valores da planilha de clientes

---
+++ lanchonete.py
@@ -0,0 +1,23 @@
+import webbrowser
+import openpyxl
+from time import sleep
+from pyautogui import *
+
+
+#webbrowser.open('https://web.whatsapp.com/')
+#sleep(30)
+
+#acessando a planilha
+workbook = openpyxl.load_workbook('clientes.xlsx')
+#acessando sheet1/contatos nos colchetes e salvando na variável pagina_clientes
+pagina_clientes = workbook['Contatos']
+#lendo informações de cada linha da planilha a partir da segunda linha
+for linha in pagina_clientes.iter_rows(min_row=2):
+    #pegando os valores de nome e telefone de cada linha a partir do indice
+    nome = linha[0].value
+    telefone= linha[1].value
+    print(nome)
+    print(telefone)
+
+#webbrowser.open(link_mensagem_whatsapp)
+#input('')
