9f3fec55edc1657df207133baa7c8e5ab4d99f7d
Gabriel Tito <160160569+JaoTitow@users.noreply.github.com>
2024-04-20 20:45:01 -0300

    updt

--- projeto-bot/lanchonete.py
+++ projeto-bot/lanchonete.py
@@ -16,8 +16,12 @@ for linha in pagina_clientes.iter_rows(min_row=2):
     #pegando os valores de nome e telefone de cada linha a partir do indice
     nome = linha[0].value
     telefone= linha[1].value
-    print(nome)
-    print(telefone)
 
+    mensagem = f"Olá {nome} seu número é {telefone}"
+    print(mensagem)
+    link_mensagem_whatsapp = f'https://web.whatsapp.com/send?phone={telefone}&text={mensagem}'
+    
+    
+    
 #webbrowser.open(link_mensagem_whatsapp)
 #input('')
